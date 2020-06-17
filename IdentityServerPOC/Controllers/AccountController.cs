3bf2800ee7aa97b8947c4ae8e94ae5c1a8ce5194
Sarbu <catalin.sarbu@cgm.com>
2020-06-17 13:00:06 +0300

    added register user

--- IdentityServerPOC/Controllers/AccountController.cs
+++ IdentityServerPOC/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using IdentityServer4.Events;
 using IdentityServer4.Services;
 using IdentityServerPOC.Infrastructure;
+using IdentityServerPOC.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -93,6 +94,32 @@ namespace IdentityServerPOC.Controllers
             await _signInManager.SignOutAsync();
             var context = await _interaction.GetLogoutContextAsync(logoutId);
             return Redirect(context.PostLogoutRedirectUri);
+        }
+
+        [HttpPost]
+        [Route("api/[controller]")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestViewModel model)
+        {
+            //var aVal = 0; var blowUp = 1 / aVal;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new AppUser { UserName = model.Email, Name = model.Name, Email = model.Email };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("userName", user.UserName));
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("name", user.Name));
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("email", user.Email));
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("role", Roles.SuperAdmin));
+
+            return Ok(new RegisterResponseViewModel(user));
         }
     }
 }
---
+++ IdentityServerPOC/Models/RegisterViewModel.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServerPOC.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServerPOC.Models
+{
+    public class RegisterRequestViewModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class RegisterResponseViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public RegisterResponseViewModel(AppUser user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            Email = user.Email;
+        }
+    }
+}
