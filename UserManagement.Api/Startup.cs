520449135465ea0ec13471a574091ad9500d24df
alex-pasaila <alexpasa2003@yahoo.com>
2020-06-25 17:59:40 +0300

    added Swagger, completed user registration

--- UserManagement.Api/Startup.cs
+++ UserManagement.Api/Startup.cs
@@ -1,12 +1,16 @@
+using IdentityServer4.AccessTokenValidation;
+using IdentityServerPOC.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using IdentityServerPOC.Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
+using UserManagement.Api.Infrastructure.Middleware;
 
 namespace UserManagement.Api
 {
@@ -23,29 +27,47 @@ namespace UserManagement.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
-           
+
             services.AddIdentity<ApplicationUser, IdentityRole>()
             .AddEntityFrameworkStores<AppIdentityDbContext>()
             .AddDefaultTokenProviders();
 
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(o =>
+            //{
+            //    o.Authority = "http://localhost:5000";
+            //    o.Audience = "digitalrecipiepoc";
+            //    o.RequireHttpsMetadata = false;
+            //});
+
             services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddIdentityServerAuthentication(options =>
             {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(o =>
-            {
-                o.Authority = "http://localhost:5000";
-                o.Audience = "digitalrecipiepoc";
-                o.RequireHttpsMetadata = false;
-            });
+                options.Authority = "http://localhost:5000";
+                    options.ApiName = "digitalrecipiepoc";
+                    options.RequireHttpsMetadata = false;
+                });
 
-            services.AddControllers();
-
-            services.AddAuthorization(options =>
+            services.AddControllers(options =>
             {
-                options.AddPolicy("ApiReader", policy => policy.RequireClaim("scope", "api.read"));
-                //options.AddPolicy("SuperAdmin", policy => policy.RequireClaim(ClaimTypes.Role, "superadmin"));
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
             });
+            services.AddSwaggerDocumentation();
+
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("ApiReader", policy => policy.RequireClaim("scope", "api.read"));
+            //    //options.AddPolicy("SuperAdmin", policy => policy.RequireClaim(ClaimTypes.Role, "superadmin"));
+            //});
 
         }
 
@@ -55,6 +77,7 @@ namespace UserManagement.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwaggerDocumentation();
             }
 
             //app.UseHttpsRedirection();
@@ -64,7 +87,7 @@ namespace UserManagement.Api
             app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseAuthentication();
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
